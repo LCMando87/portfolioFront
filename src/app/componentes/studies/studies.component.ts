25d5b27a8d8e3344115d9b871867b2c8c0abd01d
leandrocanalda@gmail.com <Administrator@MATICA009.ingematica.local>
2023-03-09 13:53:49 -0300

    Se actualiza como dinamico componente de estudios

--- src/app/componentes/studies/studies.component.ts
+++ src/app/componentes/studies/studies.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./studies.component.css']
 })
 export class StudiesComponent {
-
+  estudios = [
+    {establecimiento: "Ministerio de desarrollo productivo",
+    anoDesde: 2022,
+    anoHasta: 2023,
+    carrera: "Desarrollador Web Full Stack",
+    descripcion: "Carrera completa de desarrollador web full stack. Lenguajes: HTML, CSS, JavaScript, Typescript, Java, SQL. Metodologías: Angular, Spring"
+    }
+  ]
 }
